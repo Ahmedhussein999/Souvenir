2eaaa25756c31b9866600df01ea7a69fe115d570
Ahmed hussein <Ahmed.Hussein5@student.tdsb.on.ca>
2024-06-10 14:13:19 -0400

    Push commit

--- Scode/src/App.java
+++ Scode/src/App.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("FIrst CHECK UP");
-    }
-}
+//this is akll of my imports for this
+
+public class App {  
+    public static void main(String[] s) {  
+        JFrame frame = new JFrame("Souvnir");  
+        JPanel panel = new JPanel();  
+        panel.setLayout(new BorderLayout());  
+        
+        JLabel label = new JLabel("write your things here :");
+        JTextArea noteInput = new JTextArea(5, 20);
+        JScrollPane scrollPane = new JScrollPane(noteInput);
+        
+        // buttons
+    JButton addButton = new JButton("add da Note");  
+      
+
+        // IDK even know what this does, but this is what he put down
+        DefaultListModel<String> noteListModel = new DefaultListModel<>();
+        JList<String> noteList = new JList<>(noteListModel);
+        JScrollPane listScrollPane = new JScrollPane(noteList);
+        
+        // border for the  buttons to work and stuff
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(addButton);
+   
+
