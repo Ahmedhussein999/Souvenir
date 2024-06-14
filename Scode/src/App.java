8c768bdf6ad9f9b82566961a271ba98c2639de74
Ahmed hussein <Ahmed.Hussein5@student.tdsb.on.ca>
2024-06-14 15:13:01 -0400

    UPDATE PUSH

--- Scode/src/App.java
+++ Scode/src/App.java
@@ -1,17 +1,33 @@
 //this is akll of my imports for this
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+//___________________________________________________________
 public class App {  
     public static void main(String[] s) {  
         JFrame frame = new JFrame("Souvnir");  
+        frame.getContentPane().setBackground(Color.BLUE);
         JPanel panel = new JPanel();  
         panel.setLayout(new BorderLayout());  
         
-        JLabel label = new JLabel("write your things here :");
+        JLabel label = new JLabel("Write down your best thoguhts! :");
         JTextArea noteInput = new JTextArea(5, 20);
         JScrollPane scrollPane = new JScrollPane(noteInput);
         
         // buttons
-    JButton addButton = new JButton("add da Note");  
+    JButton addButton = new JButton("The note");  
       
 
         // IDK even know what this does, but this is what he put down
@@ -24,3 +40,31 @@ public class App {
         buttonPanel.add(addButton);
    
 
+        // making every visuable seeable 
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+        panel.add(listScrollPane, BorderLayout.EAST);
+        
+        // adding the buttons  to the program
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String noteText = noteInput.getText().trim();
+                if (!noteText.isEmpty()) {
+                    noteListModel.addElement(noteText);
+                    noteInput.setText("");
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Note is wrong forsure", "Wrong bruh", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+ 
+//program startup (aka window size/height)_________________________________________________________
+        frame.add(panel);  
+        frame.setSize(400, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);  
+    }  
+}
